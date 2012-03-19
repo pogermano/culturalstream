fa855f3707ee01d4c4533b4b6178b2f513416014
pedro germano <pogermano@gmail.com>
2012-03-19 14:17:44 -0300

    add capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,31 @@
+set :application, "culturalstream"
+set :repository,  "git@github.com:pogermano/culturalstream.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :user, "culturalstream.com"
+set :deploy_to, "/home/culturalstream.com/apps/#{application}"
+set :use_sudo, false
+set :keep_releases, 5
+
+
+
+
+
+
+role :web, "173.230.137.11"                          # Your HTTP server, Apache/etc
+role :app, "173.230.137.11"                          # This may be the same as your `Web` server
+role :db,  "173.230.137.11", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
