c374dd325b0880fc77d049cca476bc194b2797f3
pedro germano <pogermano@gmail.com>
2012-03-31 11:53:10 -0300

    testing command

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,5 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
-#Teste::Application.initialize!
-Production::Application.initialize!
+Culturalstream::Application.initialize!
+
